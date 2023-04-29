54a5e71764bdfa02109645aae7735756f1670347
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-29 15:19:19 +0200

    pokedexV2

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ function displayPokemon(pokemons) {
     for (const pokemon of pokemons) {
         pokemonBox.innerHTML += ` <details>
                                     <summary>
-                                        <span>#${pokemon.id}</span>
-                                        <img class="pokemon-img" src="${pokemon.sprites.front_default}" alt="Bulbasaur">
+                                        <span>#${numberOfPokedex(pokemon.id)}</span>
+                                        <img class="pokemon-img" src="${pokemon.sprites.front_default}" alt="${pokemon.name[0].toUpperCase()+pokemon.name.slice(1)}">
                                         <span>${pokemon.name[0].toUpperCase()+pokemon.name.slice(1)}</span>
                                         <div class="spacer"></div>
                                         ${pokemon.types.map(t => `<span class='type'>${t.type.name[0].toUpperCase()+t.type.name.slice(1)}</span>`).join(' / ')}
@@ -56,12 +56,18 @@ function createAbilitiesList(pokemon) {
 
 };
 
-function numberOfPokedex(pokemon) {
+function numberOfPokedex(pokemonId) {
 
-    let number;
-    for (const object of pokemon.id) {
-        number = '#' + 000 + object;  
+    let numberOfPokemon;
+    if(pokemonId < 10){
+        numberOfPokemon = '000' + pokemonId;
+    } else if(pokemonId >= 10 && pokemonId < 100) {
+        numberOfPokemon = '00' + pokemonId;
+    } else if(pokemonId >= 100 && pokemonId < 1000) {
+        numberOfPokemon = '0' + pokemonId;
+    } else if(pokemonId >= 1000) {
+        numberOfPokemon = pokemonId;
     };
-    return number;
-
+    return numberOfPokemon;
+    
 };
